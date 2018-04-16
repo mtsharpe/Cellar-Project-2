cd90957b920c55315802e7fa9a689f18ba9f8cfc
Matthew Sharpe <mattsharpe72@gmail.com>
2018-04-16 11:49:21 -0400

    fixing routes

--- controllers/wines.js
+++ controllers/wines.js
@@ -8,15 +8,17 @@ router.get('/', (req, res) => {
   Wine.find({}).then(wines => res.render('index', { wines }))
 })
 
-router.get('/:reds', (req, res) => {
-  Wine.find({ 'color': 'red' })
-    .then(wines => res.render('reds', { wines }))
-})
+// router.get('/reds', (req, res) => {
+//   Wine.find({ 'color': 'red' })
+//     .then(wines => res.render('reds', { wines }))
+// })
 
-router.get('/:whites', (req, res) => {
-  Wine.find({ 'color': 'white' })
-    .then(wines => res.render('whites', { wines }))
-})
+// router.get('/whites', (req, res) => {
+//   Wine.find({ 'color': 'white' })
+//     .then(wines => res.render('whites', { wines }))
+// })
+
+
 
 router.post('/', (req, res) => {
   Wine.create(req.body)
@@ -32,7 +34,7 @@ router.get('/edit/:id', (req, res) => {
     })
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:wineType/:id', (req, res) => {
   Wine.findOneAndUpdate({ _id: req.params.id }.req.body, { new: true })
     .then(wine => {
       res.redirect('/')
@@ -43,9 +45,21 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 
-router.get('/reds/:id', (req, res) => {
+router.get('/:wineType/:id', (req, res) => {
   Wine.findOne({ _id: req.params.id })
     .then(wine => res.render('show', { wine }))
+})
+
+router.get('/:wineType/:sortType?', (req, res) => {
+  let wineType = req.params.wineType.slice(0, -1)
+  Wine.find({ 'color': wineType })
+    .then(wines => {
+      if (req.params.wineType === 'reds' || req.params.wineType === 'whites' || req.params.wineType === 'others') {
+        res.render(req.params.wineType, { wines })
+      } else {
+        res.redirect('/')
+      }
+    })
 })
 
 router.delete('/:id', (req, res) => {
